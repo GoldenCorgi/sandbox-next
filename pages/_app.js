8c43faa04a406eb3082c38502d4f323366eed159
GoldenCorgi <42860714+GoldenCorgi@users.noreply.github.com>
2022-07-10 12:36:46 +0800

    yeehaw

---
+++ pages/_app.js
@@ -0,0 +1,11 @@
+import '../styles/global.css'
+// 1. import `ChakraProvider` component
+import { ChakraProvider } from '@chakra-ui/react'
+
+export default function App({ Component, pageProps }) {
+  return  <ChakraProvider>
+
+      <Component {...pageProps} />
+      </ChakraProvider>
+
+}
